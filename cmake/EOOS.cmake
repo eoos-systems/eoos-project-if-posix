f276eb8044a0e9b5c14cda7ea2b084eb54ea060c
Sergey Baigudin <baigudin@mail.ru>
2023-04-25 18:27:12 +0300

    [EOOS-37] Mutex, semaphore and stream managers developed

--- cmake/EOOS.cmake
+++ cmake/EOOS.cmake
@@ -3,5 +3,7 @@
 # @author    Sergey Baigudin, sergey@baigudin.software
 # @copyright 2020-2022, Sergey Baigudin, Baigudin Software
 
-option(EOOS_ENABLE_TESTS "Build unit tests with the project" OFF)
-option(EOOS_ENABLE_GCC_COVERAGE "Build unit tests with Gcov compiler flags" OFF)
+set(EOOS_CMAKE_ENABLE_TESTS OFF CACHE BOOL "Enable building unit test")
+set(EOOS_CMAKE_ENABLE_GCC_COVERAGE OFF CACHE BOOL "Enable building unit tests with Gcov compiler flags" OFF)
+
+set(EOOS_GLOBAL_ENABLE_NO_HEAP OFF CACHE BOOL "Enable no using heap memory")
