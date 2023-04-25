a3adf653991df31e55b34bb0d737542e886e19e7
Sergey Baigudin <baigudin@mail.ru>
2023-04-25 18:45:33 +0300

    [EOOS-36] Set variable syntax typo fixed

--- cmake/EOOS.cmake
+++ cmake/EOOS.cmake
@@ -4,6 +4,6 @@
 # @copyright 2020-2022, Sergey Baigudin, Baigudin Software
 
 set(EOOS_CMAKE_ENABLE_TESTS OFF CACHE BOOL "Enable building unit test")
-set(EOOS_CMAKE_ENABLE_GCC_COVERAGE OFF CACHE BOOL "Enable building unit tests with Gcov compiler flags" OFF)
+set(EOOS_CMAKE_ENABLE_GCC_COVERAGE OFF CACHE BOOL "Enable building unit tests with Gcov compiler flags")
 
 set(EOOS_GLOBAL_ENABLE_NO_HEAP OFF CACHE BOOL "Enable no using heap memory")
