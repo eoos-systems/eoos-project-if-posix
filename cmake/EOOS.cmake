cf2531dd9a4591068bc9d53834a505d807ec3009
Sergey Baigudin <baigudin@mail.ru>
2023-06-01 09:58:52 +0300

    [EOOS-37] Bug debug

--- cmake/EOOS.cmake
+++ cmake/EOOS.cmake
@@ -3,7 +3,7 @@
 # @author    Sergey Baigudin, sergey@baigudin.software
 # @copyright 2020-2022, Sergey Baigudin, Baigudin Software
 
-set(EOOS_CMAKE_ENABLE_TESTS OFF CACHE BOOL "Enable building unit test")
+set(EOOS_CMAKE_ENABLE_TESTS ON CACHE BOOL "Enable building unit test")
 set(EOOS_CMAKE_ENABLE_GCC_COVERAGE OFF CACHE BOOL "Enable building unit tests with Gcov compiler flags")
 
 set(EOOS_GLOBAL_ENABLE_NO_HEAP OFF CACHE BOOL "Enable no using heap memory")
